7026171d7f0e6c97bbc04c2d47ce2630e677d9e2
dalhun namgung <bnamgung@gmail.com>
2017-11-16 20:58:31 -0800

    user

--- server.js
+++ server.js
@@ -4,3 +4,17 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_dev');
+
+
+const app = require('express')();
+//create middleware
+
+app.use(require(__dirname + '/routes/auth-routes'));
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500 || err.statusCode).send(err.message || 'server error');
+
+});
+
+app.listen(process.env.PORT || 5000);
