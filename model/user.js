f002ce136c627f990b893f40fb635a49ae168ade
Devin G <engy100x@gmail.com>
2017-11-16 20:51:52 -0800

    user schema made, working on password method

---
+++ model/user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.schema ({
+
+  userName: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  passwordHash: {type: String, required: true},
+  tokenSeed: {type: String, required: true, unique: true},
+
+});
+
+userSchema.methods.createPasswordHash = function (p) {
+
+  return bcrypt.hash(p, 8)
+    .then(hash => {
+      this.passwordHash = hash;
+      return this;
+    });
+};
