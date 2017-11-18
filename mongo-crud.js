5f1ade4114fa34e0ed7c9aa9bb8e0859a0add5d1
Maks <pvl1720@gmail.com>
2017-11-18 15:30:01 -0800

    update

--- mongo-crud.js
+++ mongo-crud.js
@@ -10,6 +10,7 @@ const conenction = MongoClient.connectAsync('mongodb://localhost:27017/mongoprom
   //when passing colsole.log, the parameter will be the result of insertAsync.
   col.insertAsync({noteBody: 'this is the 1st entry'})
   .then(console.log)
+  .then(db.close.bind(db))
   .catch(console.log)
   return db;
 })
