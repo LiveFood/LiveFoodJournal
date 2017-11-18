11fcb032051e6f9756ae7a4b0603a5d5adc457cb
Maks <pvl1720@gmail.com>
2017-11-18 14:08:14 -0800

    setup connection to db

--- mongo-crud.js
+++ mongo-crud.js
@@ -7,5 +7,9 @@ const MongoClient = promAll(require('mongodb').MongoClient);
 const conenction = MongoClient.connectAsync('mongodb://localhost:27017/mongopromisify')
 .then(db => {
   const col = promAll(db.collection('notes'));
-  col.insertAsync()
+  //when passing colsole.log, the parameter will be the result of insertAsync.
+  col.insertAsync({noteBody: 'this is the 1st entry'})
+  .then(console.log)
+  .catch(console.log)
+  return db;
 })
