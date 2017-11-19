5afdbe222c7b68ffcf865d5abb2d6efbc70c43f0
Maks <pvl1720@gmail.com>
2017-11-18 16:35:54 -0800

    collection mongo

--- mongo-crud.js
+++ mongo-crud.js
@@ -4,13 +4,28 @@ const Promise = require('bluebird');
 const prom = Promise.promosify;
 const promAll = Promise.promisifyAll;
 const MongoClient = promAll(require('mongodb').MongoClient);
+const db;
+
 const conenction = MongoClient.connectAsync('mongodb://localhost:27017/mongopromisify')
-.then(db => {
-  const col = promAll(db.collection('notes'));
-  //when passing colsole.log, the parameter will be the result of insertAsync.
-  col.insertAsync({noteBody: 'this is the 1st entry'})
-  .then(console.log)
-  .then(db.close.bind(db))
-  .catch(console.log)
-  return db;
+  .then(db => {
+    db = db;
+    const col = promAll(db.collection('notes'));
+    //when passing colsole.log, the parameter will be the result of insertAsync.
+    col.insertAsync({noteBody: 'this is the 1st entry'})
+    // .then(console.log)
+    // .then(db.close.bind(db))
+      .catch(console.log)
+      .catch(db.close.bind(db))
+      return col;
+  });
+
+connection.then(col =>{
+  // const col = promAll(db.collection('notes'));
+  prom(col.find({}).toArray)()
+    .then(console.log)
 })
+.then(db.close.bind(db));
+
+
+
+//result.ops[0] -> manipulate the 1st note because it will be an array with the length of 1.
