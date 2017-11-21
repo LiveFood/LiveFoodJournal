7205697997cb74b0882a486959e8ed5eb2b18bf2
Devin G <engy100x@gmail.com>
2017-11-20 19:09:36 -0800

    user/auth router finished

--- routes/user-router.js
+++ routes/user-router.js
@@ -9,3 +9,17 @@ const bearerAuth = require('../lib/bearer-auth.js');
 
 
 const authRouter = module.exports = new Router();
+
+authRouter.post('/api/register', jsonParser, (req, res, next) => {
+
+  User.create(req.body)
+    .then(token => res.send(token))
+    .catch(next);
+});
+
+authRouter.get('/api/login', basicAuth, (req, res, next) => {
+
+  req.user.createToken()
+    .then(token => res.send(token))
+    .catch(next);
+});
