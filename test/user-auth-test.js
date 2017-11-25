b72b3c7373b76a8e232aa05e79ec9676fbee578a
Devin G <engy100x@gmail.com>
2017-11-25 15:37:42 -0800

    fixed server.js path in test file

--- test/user-auth-test.js
+++ test/user-auth-test.js
@@ -5,7 +5,7 @@ require('./lib/mock-aws');
 
 const expect = require('expect');
 const superagent = require('superagent');
-const server = require('../server.js');
+const server = require('../lib/server.js');
 const cleanDB = require('./lib/clean-db.js');
 const mockUser = require('./lib/mock-user.js');
 const faker = require('faker');
