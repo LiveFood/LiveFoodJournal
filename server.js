29a479dd89d4c17c9b4648c842047c9012b0102a
Maks <pvl1720@gmail.com>
2017-11-25 11:40:05 -0800

    fixing our all conflicts after merging

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log('Listening on port' + PORT);
+});
+app.use(require('./routes/journal-router'));
+app.use(require('./routes/recipe-router'));
+// app.use(require('./routes/user-router'));
+
+
+// const journals = require('./routes/journals');
+//
+// app.use(bodyParser.json());
+// app.use('/api', journals);
