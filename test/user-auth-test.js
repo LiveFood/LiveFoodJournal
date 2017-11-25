d0b20685eb5faabac1641aca9cd754c279902724
Devin G <engy100x@gmail.com>
2017-11-25 01:42:34 -0800

    started auth testing file

---
+++ test/user-auth-test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('dotenv').config({path: `${__dirname}/../.test.env`});
+require('./lib/mock-aws');
+
+const expect = require('expect');
+const superagent = require('superagent');
+const server = require('../server.js');
+const cleanDB = require('./lib/clean-db.js');
+const mockUser = require('./lib/mock-user.js');
+const API_URL = process.env.API_URL;
+
+describe('User Auth Testing', () => {
+  let tempUser;
+  before(server.start);
+  after(server.stop);
+  after(cleanDB);
+
+  
+});
