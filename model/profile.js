37eee4b8fa9ed2a9fda421dccd22a61e5083fadf
Maks <pvl1720@gmail.com>
2017-11-25 16:22:37 -0800

    profileSchema is setup in profileDOTjs and need to work on routes for profile now

--- model/profile.js
+++ model/profile.js
@@ -5,11 +5,10 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
   userid: {type: mongoose.Types.ObjectId, required: true},
   name: {type: String, required: true },  /* unique name not required */
-  // journal entries (array of objects)
+
   journals: [ {type: mongoose.Types.ObjectId, ref: 'Journal'} ],
 });
 
-var ObjectId = mongoose.Schema.Types.ObjectId;
-
+let ObjectId = mongoose.Schema.Types.ObjectId;
 // module.exports = mongoose.model('Profile', profileSchema);
 const Profile = module.exports = mongoose.model('Profile', profileSchema);
