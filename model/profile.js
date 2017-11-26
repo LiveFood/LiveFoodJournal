4588dd2b43a76dda1cf24249f19292d8e2f79d13
Maks <pvl1720@gmail.com>
2017-11-25 16:31:12 -0800

    profileSchema is done with usedID Name Journals as objecst array and same for Recipes

--- model/profile.js
+++ model/profile.js
@@ -6,8 +6,8 @@ const profileSchema = new mongoose.Schema({
   userid: {type: mongoose.Types.ObjectId, required: true},
   name: {type: String, required: true },  /* unique name not required */
   journals: [ {type: mongoose.Types.ObjectId, ref: 'Journal'} ],
+  recipes: [ {type: mongoose.Types.ObjectId, ref: 'Recipe'} ],
 });
 
-let ObjectId = mongoose.Schema.Types.ObjectId;
 // module.exports = mongoose.model('Profile', profileSchema);
 const Profile = module.exports = mongoose.model('Profile', profileSchema);
