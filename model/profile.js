bf91a3cc6c67fd27de5988c02e48eff8aee96ba8
Maks <pvl1720@gmail.com>
2017-11-25 16:25:03 -0800

    removed ObjectID

--- model/profile.js
+++ model/profile.js
@@ -5,10 +5,8 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
   userid: {type: mongoose.Types.ObjectId, required: true},
   name: {type: String, required: true },  /* unique name not required */
-
   journals: [ {type: mongoose.Types.ObjectId, ref: 'Journal'} ],
 });
 
-let ObjectId = mongoose.Schema.Types.ObjectId;
 // module.exports = mongoose.model('Profile', profileSchema);
 const Profile = module.exports = mongoose.model('Profile', profileSchema);
