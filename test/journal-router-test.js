cca1aa3ea39a7c98bfbbe8dc6353918abc6ea6b5
Maks <pvl1720@gmail.com>
2017-11-27 13:08:46 -0800

    Three of Three tests pass for journal router This is Good Copy here

--- test/journal-router-test.js
+++ test/journal-router-test.js
@@ -17,7 +17,7 @@ describe('testing Journal API', () => {
       return superagent.post(`${process.env.API_URL}/api/journal`)
       // .set('Authorization', `Bearer ${user.token}`)
         .send({
-          authorid: 'Max5',
+          authorid: 'Max',
           mealConsumed: 'burger',
           mealFeedback: 'tastes good!',
         })
@@ -32,7 +32,7 @@ describe('testing Journal API', () => {
       return superagent.get(`${process.env.API_URL}/api/journal`)
         .then(res => {
           expect(res.status).toEqual(200);
-          expect(res.body[0].authorid).toEqual('Max5');
+          expect(res.body[0].authorid).toEqual('Max');
           expect(res.body[0].mealConsumed).toEqual('burger');
           expect(res.body[0].mealFeedback).toEqual('tastes good!');
         });
