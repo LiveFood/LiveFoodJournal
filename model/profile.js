211ea230edbaa3322fb4059c17a099942949dd5b
Devin G <engy100x@gmail.com>
2017-11-26 21:41:43 -0800

    added photoURI to profile schema

--- model/profile.js
+++ model/profile.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
   userid: {type: mongoose.Types.ObjectId, required: true},
   name: {type: String, required: true },  /* unique name not required */
-  profilePic: {type: String},
+  photoURI: {type: String, minlength: 1},
   journals: [ {type: mongoose.Types.ObjectId, ref: 'Journal'} ],
   recipes: [ {type: mongoose.Types.ObjectId, ref: 'Recipe'} ],
 });
