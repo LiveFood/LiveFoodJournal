08d24930233aad848a0bc1bcba6b5034f8396858
dalhun namgung <bnamgung@gmail.com>
2017-12-04 17:39:57 -0800

    resolve bug

--- test/profile-router.test.js
+++ test/profile-router.test.js
@@ -2,14 +2,13 @@
 
 
 const expect = require('expect');
-const assert = require('chai').assert;
-const superagent = require('superagent');
+const assert = require('chai');
 const profile = require('../model/profile');
 
 
 describe('testing GET /api/profile', () => {
   it('should return 200 status and a user profile', () => {
-    return profile.get('/api/profile/')
+    return profile.get('../api/profile/')
     .then(res => {
       expect(res.status).toEqual(200);
       expect(res.body[0].userid).toEqual('userid');
@@ -21,7 +20,7 @@ describe('testing GET /api/profile', () => {
 
 describe('testing POST /api/profile', () => {
       it('should give us 200 in status', () => {
-        return profile.post('/api/user/')
+        return profile.post('../api/user/')
         .then(res => {
           return expect(res.status).toEqual(200);
         });
